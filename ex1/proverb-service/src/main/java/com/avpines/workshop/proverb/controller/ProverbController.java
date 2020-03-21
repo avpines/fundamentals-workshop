3c36da8ab6fa505ea7a7f59d3050be70c70f61a8
Aviad Pines <avpines@microsoft.com>
2020-03-21 14:18:47 +0200

    ex1

--- ex1/proverb-service/src/main/java/com/avpines/workshop/proverb/controller/ProverbController.java
+++ ex1/proverb-service/src/main/java/com/avpines/workshop/proverb/controller/ProverbController.java
@@ -2,14 +2,21 @@ package com.avpines.workshop.proverb.controller;
 
 import com.avpines.workshop.proverb.model.Proverb;
 import com.avpines.workshop.proverb.service.ProverbService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+@Validated
 @RestController
 public class ProverbController {
 
@@ -45,6 +52,24 @@ public class ProverbController {
         boolean inserted = proverbService.insertProverbs(proverbs);
         return new ResponseEntity<>(inserted ?
                 HttpStatus.CREATED : HttpStatus.OK);
+    }
+
+    // if a validation error occurred, ConstraintViolationException will be
+    // thrown and this method will catch it and handle it, returning a bad
+    // request (400) back to the client.
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<String>> handle(@NotNull ConstraintViolationException e) {
+        Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+        List<String> errors = violations.stream()
+                .map(
+                        v -> {
+                            String path = v.getPropertyPath().toString();
+                            // remove the name of the method itself, leave only
+                            // the actual path, e.g. 'proverbs[0].author`
+                            return path.substring(path.indexOf('.') + 1) + ": " + v.getMessage();
+                        }
+                ).collect(Collectors.toList());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
 }
